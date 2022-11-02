2a1e694de37deee74996cb677fe7fc5fc6d2e242
gorion.b <gorion.b@gmail.com>
2022-11-02 15:34:39 -0500

    Removed middle man value for settings to fix intermittent issues with settings changes not registering

--- FasterAgingMod.cs
+++ FasterAgingMod.cs
@@ -21,44 +21,44 @@ namespace FasterAging
         public override string ModIdentifier => "FasterAging";
 
         //HugsLib Settings
-        private SettingHandle<int> pawnAgingMultSetting;
-        private SettingHandle<int> pawnAgingMultAfterCutoffSetting;
-        private SettingHandle<int> pawnCutoffAgeSetting;
+        public static SettingHandle<int> pawnAgingMult;
+        public static SettingHandle<int> pawnAgingMultAfterCutoff;
+        public static SettingHandle<int> pawnCutoffAge;
 
-        private SettingHandle<int> animalAgingMultSetting;
-        private SettingHandle<int> animalAgingMultAfterCutoffSetting;
-        private SettingHandle<int> animalCutoffAgeSetting;
+        public static SettingHandle<int> animalAgingMult;
+        public static SettingHandle<int> animalAgingMultAfterCutoff;
+        public static SettingHandle<int> animalCutoffAge;
 
-        private SettingHandle<bool> enableAgeCutoffsSetting;
+        public static SettingHandle<bool> enableAgeCutoffs;
 
         //private SettingHandle<bool> enablePerPawnRateSetting; //Disabled due to not working
 
 
-        private SettingHandle<int> biosculptAgeReversalYearsSetting;
+        public static SettingHandle<int> biosculptAgeReversalYears;
 
-        private SettingHandle<int> growthVatAgeTicksPerTickSetting;
+        public static SettingHandle<int> growthVatAgeTicksPerTick;
 
 
         //Vars pulled from the HugsLib Settings
-        public static int pawnAgingMult = 1; //Multiplier to human pawn aging speed (before the age cutoff if that system is enabled)
-        public static int pawnAgingMultAfterCutoff = 1; //Multiplier to human pawn aging speed after the age cutoff
-        public static int pawnCutoffAge = 18; //Human aging rate age cutoff
+        //public static int pawnAgingMult = 1; //Multiplier to human pawn aging speed (before the age cutoff if that system is enabled)
+        //public static int pawnAgingMultAfterCutoff = 1; //Multiplier to human pawn aging speed after the age cutoff
+        //public static int pawnCutoffAge = 18; //Human aging rate age cutoff
         public static long pawnCutoffAgeTicks => ((long)pawnCutoffAge * 3600000L) + 1000L; //Cutoff age converted to ticks. 1000 ticks are added to this value as a buffer around birthdays to prevent repeatedly calling birthday code when aging is disabled.
 
-        public static int animalAgingMult = 1; //Multiplier to animal pawn aging speed (before the age cutoff if that system is enabled)
-        public static int animalAgingMultAfterCutoff = 1; //Multiplier to animal pawn aging speed after the age cutoff
-        public static int animalCutoffAge = 18; //Animal aging rate age cutoff
+        //public static int animalAgingMult = 1; //Multiplier to animal pawn aging speed (before the age cutoff if that system is enabled)
+        //public static int animalAgingMultAfterCutoff = 1; //Multiplier to animal pawn aging speed after the age cutoff
+        //public static int animalCutoffAge = 18; //Animal aging rate age cutoff
         public static long animalCutoffAgeTicks => ((long)animalCutoffAge * 3600000L) + 1000L; //Cutoff age converted to ticks. 1000 ticks are added to this value as a buffer around birthdays to prevent repeatedly calling birthday code when aging is disabled.
 
-        public static bool enableAgeCutoffs = false; //Whether the age cutoffs system is enabled
+        //public static bool enableAgeCutoffs = false; //Whether the age cutoffs system is enabled
 
-        public static bool enablePerPawnRate = false; //Whether the per-pawn rate system is enabled and its control button shown.
-
-
-        public static int biosculptAgeReversalYears = 1; //Number of years that are taken off a pawn's age at the completion of an age-reversal biosculpting process
+        //public static bool enablePerPawnRate = false; //Whether the per-pawn rate system is enabled and its control button shown.
 
 
-        public static int growthVatAgeTicksPerTick = 20; //Numer of biological age ticks gained per tick in a growth vat
+        //public static int biosculptAgeReversalYears = 1; //Number of years that are taken off a pawn's age at the completion of an age-reversal biosculpting process
+
+
+        //public static int growthVatAgeTicksPerTick = 20; //Numer of biological age ticks gained per tick in a growth vat
 
 
         //Misc
@@ -74,35 +74,28 @@ namespace FasterAging
         public override void DefsLoaded()
         {
             //Setup and get settings values
-            pawnAgingMultSetting = Settings.GetHandle<int>(settingName: "fa_pawnAgingMult", title: "fa_settings_pawnAgingMult_title".Translate(), description: "fa_settings_pawnAgingMult_description".Translate(), defaultValue: 1, Validators.IntRangeValidator(0, 99999));
-            pawnAgingMult = pawnAgingMultSetting.Value;
-
-            pawnAgingMultAfterCutoffSetting = Settings.GetHandle<int>(settingName: "fa_pawnAgingMultAfter", title: "fa_settings_pawnAgingMultAfter_title".Translate(), description: "fa_settings_pawnAgingMultAfter_description".Translate(), defaultValue: 1, Validators.IntRangeValidator(0, 99999));
-            pawnAgingMultAfterCutoffSetting.VisibilityPredicate = delegate () { return enableAgeCutoffsSetting.Value; }; //Hide if the system is disabled
-            pawnAgingMultAfterCutoff = pawnAgingMultAfterCutoffSetting.Value;
-
-            pawnCutoffAgeSetting = Settings.GetHandle<int>(settingName: "fa_pawnCutoffAge", title: "fa_settings_pawnCutoffAge_title".Translate(), description: "fa_settings_pawnCutoffAge_description".Translate(), defaultValue: 18, Validators.IntRangeValidator(0, 99999));
-            pawnCutoffAgeSetting.VisibilityPredicate = delegate () { return enableAgeCutoffsSetting.Value; }; //Hide if the system is disabled
-            pawnCutoffAge = pawnCutoffAgeSetting.Value;
+            pawnAgingMult = Settings.GetHandle<int>(settingName: "fa_pawnAgingMult", title: "fa_settings_pawnAgingMult_title".Translate(), description: "fa_settings_pawnAgingMult_description".Translate(), defaultValue: 1, Validators.IntRangeValidator(0, 99999));
+            
+            pawnAgingMultAfterCutoff = Settings.GetHandle<int>(settingName: "fa_pawnAgingMultAfter", title: "fa_settings_pawnAgingMultAfter_title".Translate(), description: "fa_settings_pawnAgingMultAfter_description".Translate(), defaultValue: 1, Validators.IntRangeValidator(0, 99999));
+            pawnAgingMultAfterCutoff.VisibilityPredicate = delegate () { return enableAgeCutoffs.Value; }; //Hide if the system is disabled
+            
+            pawnCutoffAge = Settings.GetHandle<int>(settingName: "fa_pawnCutoffAge", title: "fa_settings_pawnCutoffAge_title".Translate(), description: "fa_settings_pawnCutoffAge_description".Translate(), defaultValue: 18, Validators.IntRangeValidator(0, 99999));
+            pawnCutoffAge.VisibilityPredicate = delegate () { return enableAgeCutoffs.Value; }; //Hide if the system is disabled
+            
 
 
-
-            animalAgingMultSetting = Settings.GetHandle<int>(settingName: "fa_animalAgingMult", title: "fa_settings_animalAgingMult_title".Translate(), description: "fa_settings_animalAgingMult_description".Translate(), defaultValue: 1, Validators.IntRangeValidator(0, 99999));
-            animalAgingMult = animalAgingMultSetting.Value;
-
-            animalAgingMultAfterCutoffSetting = Settings.GetHandle<int>(settingName: "fa_animalAgingMultAfter", title: "fa_settings_animalAgingMultAfter_title".Translate(), description: "fa_settings_animalAgingMultAfter_description".Translate(), defaultValue: 1, Validators.IntRangeValidator(0, 99999));
-            animalAgingMultAfterCutoffSetting.VisibilityPredicate = delegate () { return enableAgeCutoffsSetting.Value; }; //Hide if the system is disabled
-            animalAgingMultAfterCutoff = animalAgingMultAfterCutoffSetting.Value;
-
-            animalCutoffAgeSetting = Settings.GetHandle<int>(settingName: "fa_animalCutoffAge", title: "fa_settings_animalCutoffAge_title".Translate(), description: "fa_settings_animalCutoffAge_description".Translate(), defaultValue: 18, Validators.IntRangeValidator(0, 99999));
-            animalCutoffAgeSetting.VisibilityPredicate = delegate () { return enableAgeCutoffsSetting.Value; }; //Hide if the system is disabled
-            animalCutoffAge = animalCutoffAgeSetting.Value;
+            animalAgingMult = Settings.GetHandle<int>(settingName: "fa_animalAgingMult", title: "fa_settings_animalAgingMult_title".Translate(), description: "fa_settings_animalAgingMult_description".Translate(), defaultValue: 1, Validators.IntRangeValidator(0, 99999));
+            
+            animalAgingMultAfterCutoff = Settings.GetHandle<int>(settingName: "fa_animalAgingMultAfter", title: "fa_settings_animalAgingMultAfter_title".Translate(), description: "fa_settings_animalAgingMultAfter_description".Translate(), defaultValue: 1, Validators.IntRangeValidator(0, 99999));
+            animalAgingMultAfterCutoff.VisibilityPredicate = delegate () { return enableAgeCutoffs.Value; }; //Hide if the system is disabled
+            
+            animalCutoffAge = Settings.GetHandle<int>(settingName: "fa_animalCutoffAge", title: "fa_settings_animalCutoffAge_title".Translate(), description: "fa_settings_animalCutoffAge_description".Translate(), defaultValue: 18, Validators.IntRangeValidator(0, 99999));
+            animalCutoffAge.VisibilityPredicate = delegate () { return enableAgeCutoffs.Value; }; //Hide if the system is disabled
+            
 
 
-
-            enableAgeCutoffsSetting = Settings.GetHandle<bool>(settingName: "fa_enableAgeCutoffs", title: "fa_settings_enableAgeCutoffs_title".Translate(), description: "fa_settings_enableAgeCutoffs_description".Translate(), defaultValue: false);
-            enableAgeCutoffs = enableAgeCutoffsSetting.Value;
-
+            enableAgeCutoffs = Settings.GetHandle<bool>(settingName: "fa_enableAgeCutoffs", title: "fa_settings_enableAgeCutoffs_title".Translate(), description: "fa_settings_enableAgeCutoffs_description".Translate(), defaultValue: false);
+            
 
 
             //Disabled due to not working
@@ -112,14 +105,12 @@ namespace FasterAging
 
 
 
-            biosculptAgeReversalYearsSetting = Settings.GetHandle<int>(settingName: "fa_biosculptAgeReversalYears", title: "fa_settings_biosculptAgeReversalYears_title".Translate(), description: "fa_settings_biosculptAgeReversalYears_description".Translate(), defaultValue: 1, Validators.IntRangeValidator(1, 100));
-            biosculptAgeReversalYearsSetting.VisibilityPredicate = delegate () { return ModsConfig.IdeologyActive; }; //Only show when the user has Ideology
-            biosculptAgeReversalYears = biosculptAgeReversalYearsSetting.Value;
+            biosculptAgeReversalYears = Settings.GetHandle<int>(settingName: "fa_biosculptAgeReversalYears", title: "fa_settings_biosculptAgeReversalYears_title".Translate(), description: "fa_settings_biosculptAgeReversalYears_description".Translate(), defaultValue: 1, Validators.IntRangeValidator(1, 100));
+            biosculptAgeReversalYears.VisibilityPredicate = delegate () { return ModsConfig.IdeologyActive; }; //Only show when the user has Ideology
+            
 
-
-            growthVatAgeTicksPerTickSetting = Settings.GetHandle<int>(settingName: "fa_growthVatAgeTicksPerTick", title: "fa_settings_growthVatAgeTicksPerTick_title".Translate(), description: "fa_settings_growthVatAgeTicksPerTick_description".Translate(), defaultValue: 20, Validators.IntRangeValidator(1, 99999));
-            growthVatAgeTicksPerTickSetting.VisibilityPredicate = delegate () { return ModsConfig.BiotechActive; }; //Only show when the user has Biotech
-            growthVatAgeTicksPerTick = growthVatAgeTicksPerTickSetting.Value;
+            growthVatAgeTicksPerTick = Settings.GetHandle<int>(settingName: "fa_growthVatAgeTicksPerTick", title: "fa_settings_growthVatAgeTicksPerTick_title".Translate(), description: "fa_settings_growthVatAgeTicksPerTick_description".Translate(), defaultValue: 20, Validators.IntRangeValidator(1, 99999));
+            growthVatAgeTicksPerTick.VisibilityPredicate = delegate () { return ModsConfig.BiotechActive; }; //Only show when the user has Biotech
         }
 
         /// <summary>
@@ -137,36 +128,6 @@ namespace FasterAging
         }
 
         /// <summary>
-        /// Runs whenever the user changes a setting during runtime.
-        /// Adjusts the internal variables to match the new settings.
-        /// </summary>
-        public override void SettingsChanged()
-        {
-            //TODO there is apparently a better way of doing this via setting's ValueChanged event, but I can't get that working right now. Come back to this in the future.
-
-
-            pawnAgingMult = pawnAgingMultSetting.Value;
-            pawnAgingMultAfterCutoff = pawnAgingMultAfterCutoffSetting.Value;
-            pawnCutoffAge = pawnCutoffAgeSetting.Value;
-
-            animalAgingMult = animalAgingMultSetting.Value;
-            animalAgingMultAfterCutoff = animalAgingMultAfterCutoffSetting.Value;
-            animalCutoffAge = animalCutoffAgeSetting.Value;
-
-            enableAgeCutoffs = enableAgeCutoffsSetting.Value;
-
-            //Disabled due to not working
-            //enablePerPawnRate = enablePerPawnRateSetting.Value;
-            //if (!enablePerPawnRate && perPawnRates != null) perPawnRates.Clear(); //Removes all previously selected custom rates when toggling off - this allows the user to reset their choices if they did something silly.
-
-
-            biosculptAgeReversalYears = biosculptAgeReversalYearsSetting.Value;
-
-
-            growthVatAgeTicksPerTick = growthVatAgeTicksPerTickSetting.Value;
-        }
-
-        /// <summary>
         /// Gets the aging rate multiplier value for the input pawn, based on the mod's settings, and the pawn's type and age, and whether the pawn has a per-pawn rate set
         /// </summary>
         /// <param name="pawn">Pawn to determine the age rate multiplier for</param>
@@ -174,14 +135,15 @@ namespace FasterAging
         public static int GetPawnAgingMultiplier(Pawn pawn)
         {
             //First check if the pawn has a per-pawn rate selected and use it if they do
-            if (enablePerPawnRate)
-            {
-                int perPawnRate = GetPerPawnAgingMultiplier(pawn);
-                if (perPawnRate != -1)
-                {
-                    return perPawnRate;
-                }
-            }
+            //Disabled as it is not working
+            //if (enablePerPawnRate)
+            //{
+            //    int perPawnRate = GetPerPawnAgingMultiplier(pawn);
+            //    if (perPawnRate != -1)
+            //    {
+            //        return perPawnRate;
+            //    }
+            //}
 
             if (!pawn.RaceProps.Humanlike)
             {
@@ -217,27 +179,27 @@ namespace FasterAging
             }
         }
 
+        //Below are disabled due to per pawn system not working
+        ///// <summary>
+        ///// Returns the per-pawn custom aging rate associated with the input Pawn, if it exists. If it is not found, returns -1.
+        ///// </summary>
+        ///// <param name="pawn"></param>
+        ///// <returns></returns>
+        //public static int GetPerPawnAgingMultiplier(Pawn pawn)
+        //{
+        //    if (perPawnRates.ContainsKey(pawn.GetUniqueLoadID())) return perPawnRates[pawn.GetUniqueLoadID()];
 
-        /// <summary>
-        /// Returns the per-pawn custom aging rate associated with the input Pawn, if it exists. If it is not found, returns -1.
-        /// </summary>
-        /// <param name="pawn"></param>
-        /// <returns></returns>
-        public static int GetPerPawnAgingMultiplier(Pawn pawn)
-        {
-            if (perPawnRates.ContainsKey(pawn.GetUniqueLoadID())) return perPawnRates[pawn.GetUniqueLoadID()];
+        //    return -1;
+        //}
 
-            return -1;
-        }
-
-        /// <summary>
-        /// Sets the per-pawn custom aging rate for the input pawn to the input multiplier.
-        /// </summary>
-        /// <param name="pawn"></param>
-        /// <param name="mult"></param>
-        public static void SetPerPawnAgingMultiplier(Pawn pawn, int mult)
-        {
-            perPawnRates[pawn.GetUniqueLoadID()] = mult;
-        }
+        ///// <summary>
+        ///// Sets the per-pawn custom aging rate for the input pawn to the input multiplier.
+        ///// </summary>
+        ///// <param name="pawn"></param>
+        ///// <param name="mult"></param>
+        //public static void SetPerPawnAgingMultiplier(Pawn pawn, int mult)
+        //{
+        //    perPawnRates[pawn.GetUniqueLoadID()] = mult;
+        //}
     }
 }
--- FasterAgingPerPawnGizmoPatch.cs
+++ FasterAgingPerPawnGizmoPatch.cs
@@ -17,41 +17,42 @@ namespace FasterAging
         public static Texture2D perPawnGizmoIcon = ContentFinder<Texture2D>.Get("UI/Commands/FAPerPawnGizmo", true);
 
 
-        /// <summary>
-        /// If the per-pawn aging system is enabled, then patch adds a gizmo (pawn control bar button) to the pawn that opens a ui to change that rate for this pawn.
-        /// </summary>
-        /// <param name="__result"></param>
-        /// <param name="__instance"></param>
-        /// <returns></returns>
-        [HarmonyPostfix]
-        public static IEnumerable<Gizmo> AddPerPawnRateControlGizmo(IEnumerable<Gizmo> __result, Pawn __instance)
-        {
-            //Copied from another mod -- I don't really understand how yield works but I think it returns all of the prior gizmos already stored in __result before going on to building and returning this mod's gizmo
-            //This seemed like a cleaner way of doing things than the other way I found.
-            foreach (var gizmo in __result)
-            {
-                yield return gizmo;
-            }
+        //Below disabled as per pawn system is not working
+        ///// <summary>
+        ///// If the per-pawn aging system is enabled, then patch adds a gizmo (pawn control bar button) to the pawn that opens a ui to change that rate for this pawn.
+        ///// </summary>
+        ///// <param name="__result"></param>
+        ///// <param name="__instance"></param>
+        ///// <returns></returns>
+        //[HarmonyPostfix]
+        //public static IEnumerable<Gizmo> AddPerPawnRateControlGizmo(IEnumerable<Gizmo> __result, Pawn __instance)
+        //{
+        //    //Copied from another mod -- I don't really understand how yield works but I think it returns all of the prior gizmos already stored in __result before going on to building and returning this mod's gizmo
+        //    //This seemed like a cleaner way of doing things than the other way I found.
+        //    foreach (var gizmo in __result)
+        //    {
+        //        yield return gizmo;
+        //    }
 
-            if (FasterAgingMod.enablePerPawnRate)
-            {
-                yield return new Command_Action
-                {
-                    defaultLabel = "fa_perPawnRateGizmoLabel".Translate(),
-                    defaultDesc = "fa_perPawnRateGizmoDesc".Translate(),
-                    icon = perPawnGizmoIcon,
-                    action = delegate
-                    {
-                        //Open a dialog box with a slider to select the aging rate
-                        string textGetter(int x) => (x != -1 ? "fa_perPawnDialogText".Translate(x) : "fa_perPawnDialogText_global".Translate(x));
-                        Dialog_Slider window = new Dialog_Slider(textGetter, -1, 100, delegate (int value)
-                        {
-                            FasterAgingMod.SetPerPawnAgingMultiplier(__instance, value);
-                        }, FasterAgingMod.GetPerPawnAgingMultiplier(__instance));
-                        Find.WindowStack.Add(window);
-                    },
-                };
-            }
-        }
+        //    if (FasterAgingMod.enablePerPawnRate)
+        //    {
+        //        yield return new Command_Action
+        //        {
+        //            defaultLabel = "fa_perPawnRateGizmoLabel".Translate(),
+        //            defaultDesc = "fa_perPawnRateGizmoDesc".Translate(),
+        //            icon = perPawnGizmoIcon,
+        //            action = delegate
+        //            {
+        //                //Open a dialog box with a slider to select the aging rate
+        //                string textGetter(int x) => (x != -1 ? "fa_perPawnDialogText".Translate(x) : "fa_perPawnDialogText_global".Translate(x));
+        //                Dialog_Slider window = new Dialog_Slider(textGetter, -1, 100, delegate (int value)
+        //                {
+        //                    FasterAgingMod.SetPerPawnAgingMultiplier(__instance, value);
+        //                }, FasterAgingMod.GetPerPawnAgingMultiplier(__instance));
+        //                Find.WindowStack.Add(window);
+        //            },
+        //        };
+        //    }
+        //}
     }
 }
